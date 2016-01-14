4266b7b59b02fcb5d9a8d69e0070624484aee04f
phoenix <pho.coderzhang@gmail.com>
2016-01-14 18:44:51 +0800

    fix

--- src/rocks/pho/btc_robot/core.clj
+++ src/rocks/pho/btc_robot/core.clj
@@ -1,10 +1,51 @@
 (ns rocks.pho.btc-robot.core
   (:gen-class)
 
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+(defn deal-one-kline
+  "add other value for one kline"
+  [data]
+  (assoc data
+         :up (if (> (:end-price data) (:start-price data))
+                    true
+                    false)
+         ))
+
+(defn parse-kline-data
+  "parse kline data from array to map"
+  [data]
+  {:datetime (let [datetime (nth data 0)]
+               (str (.substring datetime 0 4)
+                    "-"
+                    (.substring datetime 4 6)
+                    "-"
+                    (.substring datetime 6 8)
+                    " "
+                    (.substring datetime 8 10)
+                    ":"
+                    (.substring datetime 10 12)))
+   :start-price (nth data 1)
+   :top-price (nth data 2)
+   :low-price (nth data 3)
+   :end-price (nth data 4)
+   :volume (nth data 5)})
+
+(defn get-kline
+  "get kline 001 005 ..."
+  [type]
+  (let [url (case type
+              "001" (str "http://api.huobi.com/staticmarket/btc_kline_" type "_json.js")
+              (throw Exception "kline type error: " type))]
+    (json/read-str (:body (client/get url)))))
+
+(defn get-last-kline
+  "get last n kline by type"
+  [type last-n]
+  (reverse (map #(parse-kline-data %) (take last-n (reverse (get-kline type))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
-  (println (client/get "http://api.huobi.com/staticmarket/btc_kline_001_json.js")))
+  (println "Hello, World!"))
---
+++ test/rocks/pho/btc_robot/core_test.clj
@@ -0,0 +1,8 @@
+(ns rocks.pho.btc-robot.core-test
+  (:require [clojure.test :refer :all]
+            [rocks.pho.btc-robot.core :refer :all]))
+
+(deftest a-test
+  (testing "get kline"
+    (prn (get-last-kline "001" 10))
+    (is (= 1 1))))
