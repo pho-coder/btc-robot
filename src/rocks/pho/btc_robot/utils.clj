7d4be20a2c9a6a038b379785da16cdaaef20d85d
phoenix <pho.coderzhang@gmail.com>
2016-01-19 16:08:04 +0800

    fix

--- src/rocks/pho/btc_robot/utils.clj
+++ src/rocks/pho/btc_robot/utils.clj
@@ -1,6 +1,7 @@
 (ns rocks.pho.btc-robot.utils
   (:require [clj-http.client :as client]
-            [digest :as digest]))
+            [digest :as digest]
+            [clojure.data.json :as json]))
 
 (defn get-account-info
   "FIXME"
@@ -13,9 +14,9 @@
                       "&method=get_account_info&secret_key="
                       secret_key)
         sign (digest/md5 sign-str)]
-    (client/post "https://api.huobi.com/apiv3"
-                 {:headers {"Content-Type" "application/x-www-form-urlencoded"}
-                  :form-params {:method "get_account_info"
-                                :access_key access_key
-                                :created unix-time
-                                :sign sign}})))
+    (json/read-str (:body (client/post "https://api.huobi.com/apiv3"
+                                       {:headers {"Content-Type" "application/x-www-form-urlencoded"}
+                                        :form-params {:method "get_account_info"
+                                                      :access_key access_key
+                                                      :created unix-time
+                                                      :sign sign}})))))
